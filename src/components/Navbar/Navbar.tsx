3bae28b6dd2751f3aad8d0d6d4c11960dce3fb0d
adriscicchitano <ascicchitano@alumnos.exa.unicen.edu.ar>
2022-07-02 16:20:38 -0300

    add burguer menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,42 +2,57 @@ import React from 'react';
 import logo from "../../img/logo.png"
 import burguer from "../../img/burguer.png"
 import classes from './styles.module.scss'
+import weDevelopLogo from '../../img/isologo.png'
+import closeButton from '../../img/cross.png'
+import { useState } from 'react';
 
 export default function Navbar() {
 
-
+  const [burguerOpen, setBurguerOpen] = useState(false)
 
   return(
-    <div className={classes.navbar}>
-      <div className={classes.logo}>
-        <button>
-          <img src={logo} alt=""/>
-        </button>
+    <>
+      <div className={classes.navbar} style={{display: burguerOpen ? "none" : undefined}}>
+        <div className={classes.logo}>
+          <button>
+            <img src={logo} alt=""/>
+          </button>
+        </div>
+        <div className={classes.burguerLogo}>
+          <button onClick={() => {setBurguerOpen(!burguerOpen)}}>
+            <img src={burguer} alt=""/>
+          </button>
+        </div>
+        <div className={classes.menu}>
+          <button onClick={() => {console.log("456")}}>Services</button>
+          <button>About Us</button>
+          <button>Testimonials</button>
+          <button>Blog</button>
+          <button>Career</button>
+        </div>
+        <div className={classes.getInTouch}>
+          <button>Get in Touch</button>
+        </div>
       </div>
-      <div className={classes.menu}>
-        <button onClick={() => {console.log("456")}}>Services</button>
-        <button>About Us</button>
-        <button>Testimonials</button>
-        <button>Blog</button>
-        <button>Career</button>
+      <div className={classes.burguer} style={{display: !burguerOpen ? "none" : undefined}}>
+        <div className={classes.navbarHeader}>
+          <button><img src={weDevelopLogo} alt=""/></button>
+          <button onClick={() => {setBurguerOpen(!burguerOpen)}}>
+            <img src={closeButton} alt=""/>
+          </button>
+        </div>
+        <div className={classes.burguerContainer}>
+          <button className={classes.navigationElement}>About Us</button>
+          <hr className={classes.linkSeparator}/>
+          <button className={classes.navigationElement}>Testimonials</button>
+          <hr className={classes.linkSeparator}/>
+          <button className={classes.navigationElement}>Blog</button>
+          <hr className={classes.linkSeparator}/>
+          <button className={classes.navigationElement}>Career</button>
+          <hr className={classes.linkSeparator}/>
+          <button className={classes.dropdownGetInTouch}>Get in Touch</button>
+        </div>
       </div>
-      <div className={classes.getInTouch}>
-        <button>Get in Touch</button>
-      </div>
-      <nav className={classes.burguer} onClick={console.log}>
-        <button><img src={burguer} alt=""/></button>
-        <ul className={classes.burguerDropdown}>
-          <li><a href='/'>About Us</a></li>
-          <li><a href='/'>Testimonials</a></li>
-          <li><a href='/'>Blog</a></li>
-          <li><a href='/'>Career</a></li>
-          <li>
-            <div className={classes.getInTouch}>
-              <button>Get in Touch</button>
-            </div>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    </>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.scss'
 import Home from './pages/Home/Home';
 import reportWebVitals from './reportWebVitals';
-import classes from './components/Navbar/styles.module.scss'
-import burguer from './img/burguer.png'
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <nav className={classes.burguer} onClick={console.log}>
-        <button><img src={burguer} alt=""/></button>
-        <ul className={classes.burguerDropdown}>
-          <li><a className={classes.dropdownElement} href='/'>About Us</a></li>
-          <li><a className={classes.dropdownElement} href='/'>Testimonials</a></li>
-          <li><a className={classes.dropdownElement} href='/'>Blog</a></li>
-          <li><a className={classes.dropdownElement} href='/'>Career</a></li>
-          <li>
-              <button className={classes.dropdowngetInTouch}>Get in Touch</button>
-          </li>
-        </ul>
-      </nav>
+    <Home/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass button function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
